8761a1f1e5870d5e4c227becf4267568aeebf12e
barbara rebelo <barbaramrebelo51@gmail.com>
2020-09-01 17:09:13 +0100

    updated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :topics, only: [:index, :show] do 
+  resources :topics, only: [:index, :show] do
     resources :themes
   end
-  
+
   resources :themes do
     resources :comments
   end
 
   resources :topics, only: [:index, :show] do
-    resources :articles, only [:index, :show]
+    resources :articles, only: [:index, :show]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
