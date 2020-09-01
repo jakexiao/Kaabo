8b060330c59945681fbbb5997dcff410a7157838
Daniel Schiavoni <daniel.schia@gmail.com>
2020-09-01 14:26:00 +0100

    themes controller done

---
+++ app/controllers/themes_controller.rb
@@ -0,0 +1,43 @@
+class ThemesController < ApplicationController
+  def new
+      @topic = Topic.find(params[:topic_id])
+      @user = User.find(params[:user_id])
+      @theme = Theme.new
+  end
+
+  def create
+    @theme = Theme.new(theme_params)
+    @topic = Topic.find(params[:topic_id])
+    @user = User.find(params[:user_id])
+    @theme.topic = @topic
+    @theme.user = @user
+    @theme.save
+  end
+
+  def index
+    @topic = Topic.find(params[:topic_id])
+    @themes = Theme.all
+  end
+
+  def show
+    @theme = Theme.find(params[:id])
+    @comments = Comment.all
+  end
+
+  def destroy
+    @theme = Theme.find(params[:id])
+    @theme.destroy
+  end
+
+  def update
+    @theme = Theme.find(params[:id])
+    @theme.update(theme_params)
+  end
+
+  private
+
+  def theme_params
+    params.require(:theme).permit(:title, :date)
+  end
+  
+end
