1fd4ac8d1f8ea46d71da48cf0de0e498468e3db8
Jake Xiao <xiaosifan0223@hotmail.com>
2020-09-01 13:33:36 +0100

    comments controller added and cloudary setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   has_many :themeupvotes
   has_many :commentupvotes
 
+  has_one_attached :avatar
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
