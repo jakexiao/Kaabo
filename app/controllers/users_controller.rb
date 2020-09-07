c945fa11ee99b94c121d83a5ce12f2500b27a73b
Daniel Schiavoni <daniel.schia@gmail.com>
2020-09-07 11:01:34 +0100

    avatar added and working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:username, :password, :avatar)
+      params.require(:user).permit(:username, :password, :photo)
     end
 end
