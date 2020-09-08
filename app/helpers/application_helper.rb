067266633b878983bb3e316775f0623401201e31
Daniel Schiavoni <daniel.schia@gmail.com>
2020-09-08 15:09:10 +0100

    search resolved

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def find_article_topic(article_id)
+    Article.find(article_id).topic
+  end
+
+  def find_theme_topic(theme_id)
+    Theme.find(theme_id).topic
+  end
 end
