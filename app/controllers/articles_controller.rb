7fc929f798355cfd6473e9ce3530d97fa0ef6278
Daniel Schiavoni <daniel.schia@gmail.com>
2020-09-08 17:32:05 +0100

    article pagination working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   def index
     @topic = Topic.find(params[:topic_id])
     @articles = Article.all
+    @articles = @articles.page(params[:page]).per(10)
   end
 
   def show
