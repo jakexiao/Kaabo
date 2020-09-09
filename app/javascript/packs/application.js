4532b1f9acdd78e68c9c57adda765bc40a3bbebf
Jake Xiao <xiaosifan0223@hotmail.com>
2020-09-09 14:56:26 +0100

    filters selected

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,10 +28,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { type_filter } from "../components/filter";
 import { masterSearch } from "../components/master_search";
+import { threadFilter } from "../components/thread_filter";
 
 document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   type_filter();
   masterSearch();
+  threadFilter();
   // initSelect2();
 });
