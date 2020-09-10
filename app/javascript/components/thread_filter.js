d94119a5520b1396b598ce29d23ac91fc157a69b
Jake Xiao <xiaosifan0223@hotmail.com>
2020-09-10 18:37:54 +0100

    filter colors work now

--- app/javascript/components/thread_filter.js
+++ app/javascript/components/thread_filter.js
@@ -1,17 +1,17 @@
-export function threadFilter() {
-  const filterbutton = document.querySelectorAll(".btn-outline-secondary");
+// export function threadFilter() {
+//   const filterbutton = document.querySelectorAll(".btn-outline-secondary");
 
-  filterbutton.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      // event.currentTarget.classList.remove("btn-outline-secondary");
-      event.currentTarget.classList.toggle("btn-outline-warning");
-    });
-  });
+//   filterbutton.forEach((button) => {
+//     button.addEventListener("click", (event) => {
+// event.currentTarget.classList.remove("btn-outline-secondary");
+// event.currentTarget.classList.toggle("btn-outline-warning");
+//   });
+// });
 
-  //   const queryString = window.location.search;
-  //   if (queryString.includes('likes')) {
-  //     document.getElementById('likes').classList.add("btn-outline-warning")
-  //   } else if (queryString.includes('comments')) {
-  //     document.getElementById('comments').classList.add("btn-outline-warning")
-  //   }
-}
+//   const queryString = window.location.search;
+//   if (queryString.includes('likes')) {
+//     document.getElementById('likes').classList.add("btn-outline-warning")
+//   } else if (queryString.includes('comments')) {
+//     document.getElementById('comments').classList.add("btn-outline-warning")
+//   }
+// }
