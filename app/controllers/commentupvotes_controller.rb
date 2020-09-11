fe5958463750e967c4561b6e430ca7f4026b6d0f
Jake Xiao <xiaosifan0223@hotmail.com>
2020-09-11 12:13:39 +0100

    refresh problems fixed, form button color changed to yellow, language settings done

--- app/controllers/commentupvotes_controller.rb
+++ app/controllers/commentupvotes_controller.rb
@@ -2,12 +2,13 @@ class CommentupvotesController < ApplicationController
     def create
         @comment = Comment.find(params[:comment_id])
         @commentupvote = Commentupvote.create(comment: @comment, user:current_user)
-        redirect_to topic_theme_path(@comment.theme.topic, @comment.theme)
+        # redirect_to topic_theme_path(@comment.theme.topic, @comment.theme)
       end
       
       def destroy
         @commentupvote = Commentupvote.find(params[:id])
+        @comment = @commentupvote.comment
         @commentupvote.destroy
-        redirect_to topic_theme_path(@commentupvote.comment.theme.topic, @commentupvote.comment.theme)
+        # redirect_to topic_theme_path(@commentupvote.comment.theme.topic, @commentupvote.comment.theme)
       end
 end
